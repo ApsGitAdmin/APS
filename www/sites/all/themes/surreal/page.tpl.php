a119a304b288f792c3e7e1dc8190bedeee535828
Moz <andrew.morris@thinkaps.com>
2015-11-30 14:42:07 +0000

    XMAS Photo competition files V2

--- www/sites/all/themes/surreal/page.tpl.php
+++ www/sites/all/themes/surreal/page.tpl.php
@@ -26,13 +26,15 @@
 </nav>
 <!-- End Navigation --> 
 <div id="page">
+    <!-- XMAS LINK BLOCK -->
+    <div id="xmas" class="hidden-phone"><a href="http://www.thinkaps.com/christmas-photo-entries" title="Christmas Photo Entry!"><img src="<?php global $base_path; print $base_path . drupal_get_path('theme', 'surreal'); ?>/images/Xmas-Frame.png" /></a></div>
+    <div id="xmas-phone" class="visible-phone"><a href="http://www.thinkaps.com/christmas-photo-entries" title="Christmas Photo Entry!"><img src="<?php global $base_path; print $base_path . drupal_get_path('theme', 'surreal'); ?>/images/Xmas-Frame-Mobile.png" /></a></div>
+    <!-- XMAS LINK BLOCK -->
+    
   <div id="homepage" class="homepage section">
     <div id="r0"></div>
     
     <!-- Region Hero -->  
-    <!-- XMAS LINK BLOCK -->
-    <div id="xmas" class="hidden-phone"><a href="http://www.thinkaps.com/christmas-photo-entries" title="Christmas Photo Entry!"><img src="<?php global $base_path; print $base_path . drupal_get_path('theme', 'surreal'); ?>/images/Xmas-Frame.png" /></a></div>
-    <div id="xmas-phone" class="visible-phone"><a href="http://www.thinkaps.com/christmas-photo-entries" title="Christmas Photo Entry!"><img src="<?php global $base_path; print $base_path . drupal_get_path('theme', 'surreal'); ?>/images/Xmas-Frame-Mobile.png" /></a></div>
     <?php if ($page['hero']): ?>
     <?php print render($page["hero"]); ?>
     <?php endif; ?>
