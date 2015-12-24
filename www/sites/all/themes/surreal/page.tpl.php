a107f9ad4903ed685b46a9f1acbe290bff47ec16
Moz <andrew.morris@thinkaps.com>
2015-12-24 10:30:43 +0000

    Remove Xmas link

--- www/sites/all/themes/surreal/page.tpl.php
+++ www/sites/all/themes/surreal/page.tpl.php
@@ -25,12 +25,7 @@
   </div>
 </nav>
 <!-- End Navigation --> 
-<div id="page">
-    <!-- XMAS LINK BLOCK -->
-    <div id="xmas" class="hidden-phone"><a href="<?php global $base_url; print $base_url; ?>/christmas-photo-entries" title="Christmas Photo Entry!"><img src="<?php global $base_path; print $base_path . drupal_get_path('theme', 'surreal'); ?>/images/Xmas-Frame.png" /></a></div>
-    <div id="xmas-phone" class="visible-phone"><a href="<?php global $base_url; print $base_url; ?>/christmas-photo-entries" title="Christmas Photo Entry!"><img src="<?php global $base_path; print $base_path . drupal_get_path('theme', 'surreal'); ?>/images/Xmas-Frame-Mobile.png" /></a></div>
-    <!-- XMAS LINK BLOCK -->
-    
+<div id="page">  
   <div id="homepage" class="homepage section">
     <div id="r0"></div>
     
