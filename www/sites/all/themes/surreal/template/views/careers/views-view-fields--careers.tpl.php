0d52d708c3109e7e111ba9723329faa54a542ca5
Moz <andrew.morris@thinkaps.com>
2016-01-15 14:55:30 +0000

    Careers block update v2

--- www/sites/all/themes/surreal/template/views/careers/views-view-fields--careers.tpl.php
+++ www/sites/all/themes/surreal/template/views/careers/views-view-fields--careers.tpl.php
@@ -4,28 +4,25 @@
   $Tagline = $fields['field_tagline']->content; 
   $Body = $fields['body']->content;
   $Link = $fields['field_workable']->content;
-  
-  $Anchor = str_replace(' ', '-', strtolower($fields['title']->raw)).'-anchor';
 ?>
 
-<a name='<?php print $Anchor; ?>' id='<?php print $Anchor; ?>'>
-  <div class="careers <?php print $zebra; ?> clearfix">
-    <div class="careers-header">
-      <h1 class="minimo-bold" style='color: <?php print $Colour; ?>'>
-        <?php 
-          $Title = explode(" ", $Title->content);
-          print '<span class="minimo-light uppercase">' . array_shift($Title) . '</span>' . trim(implode(" ", $Title));
-        ?>
-      </h1>
-      <p class="tagline">
-        <span style='background-color: <?php print $Colour; ?>' class="minimo-light uppercase"><?php print $Tagline;?></span>
-      </p>
-    </div>
-    <div class="careers-text">
-      <?php print $Body; ?>
-      <p class="workable" style='color: <?php print $Colour; ?>'>
-        <?php print $Link; ?>
-      </p>
-    </div>
+
+<div class="careers <?php print $zebra; ?> clearfix">
+  <div class="careers-header">
+    <h1 class="minimo-bold" style='color: <?php print $Colour; ?>'>
+      <?php 
+        $Title = explode(" ", $Title->content);
+        print '<span class="minimo-light uppercase">' . array_shift($Title) . '</span>' . trim(implode(" ", $Title));
+      ?>
+    </h1>
+    <p class="tagline">
+      <span style='background-color: <?php print $Colour; ?>' class="minimo-light uppercase"><?php print $Tagline;?></span>
+    </p>
   </div>
-</a>
+  <div class="careers-text">
+    <?php print $Body; ?>
+    <p class="workable minimo-light uppercase" style='background-color: <?php print $Colour; ?>'>
+      <?php print $Link; ?>
+    </p>
+  </div>
+</div>
