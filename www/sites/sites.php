a693cca0a1a0ef0ecada9c00e5e9ba477971f9d7
Emlyn Kinzett <emlyn.kinzett@codeenigma.com>
2016-05-26 15:51:23 +0100

    #16260 - Adding a sites.php file

---
+++ www/sites/sites.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Local VM.
+ */
+# Player.
+$sites['player.dev.thinkaps.com'] = 'player';
+
+# Signage.
+$sites['signage.dev.thinkaps.com'] = 'signage';
+
+/**
+ * Dev sites.
+ */
+
+# Note here, you folder naming can be whatever,
+# and you can point several hosts to the same one.
+
+# Player.
+$sites['player.master.aps-app1.codeenigma.net'] = 'player';
+$sites['aps.player.master.aps-app1.codeenigma.net'] = 'player';
+
+# Signage.
+$sites['signage.master.aps-app1.codeenigma.net'] = 'signage';
+$sites['signage.aps.master.aps-app1.codeenigma.net'] = 'signage';
+
+/**
+ * Prod sites
+ */
+
+# Player.
+$sites['player.thinkaps.com'] = 'player';
+
+# Signage.
+$sites['signage.thinkaps.com'] = 'signage';
