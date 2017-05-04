eccdaabeed6b957831915572377a06cfd0792006
Andrew-aps <andrew.morris@thinkaps.com>
2017-05-04 11:46:16 +0100

    HTTP Refresh V2

--- www/sites/signage/themes/aps_digitalsignage/template.php
+++ www/sites/signage/themes/aps_digitalsignage/template.php
@@ -25,14 +25,21 @@ function aps_digitalsignage_preprocess_node(&$vars) {
 }
 
 function aps_digitalsignage_preprocess_html(&$vars) {
-	dpm($vars);
-	$http_equiv = array(
-	    '#type' => 'html_tag',
-	    '#tag' => 'meta',
-	    '#attributes' => array(
-		    'http-equiv' => 'Refresh',
-		    'content' => '300',
-		)
-	);
-	//drupal_add_html_head($http_equiv, 'http_equiv');
+	if ($vars['page']) {
+		if ($vars['page']['#views_contextual_links_info']) {
+			if ($vars['page']['#views_contextual_links_info']['views_ui']) {
+				if ($vars['page']['#views_contextual_links_info']['views_ui']['view_name'] == 'cluster_display') && $vars['page']['#views_contextual_links_info']['views_ui']['view_display_id'] == 'room_list') {
+					$http_equiv = array(
+					    '#type' => 'html_tag',
+					    '#tag' => 'meta',
+					    '#attributes' => array(
+						    'http-equiv' => 'Refresh',
+						    'content' => '300',
+						)
+					);
+					drupal_add_html_head($http_equiv, 'http_equiv');
+				}
+			}
+		}
+	}
 }
