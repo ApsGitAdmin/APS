79f7893338389948ca3990905d26dc0a1a8cb530
Alex-APS <alex.hill@thinkaps.com>
2018-01-03 15:56:37 +0000

    revert DARK to LIGHT

--- www/sites/all/themes/surreal/template/views/about/views-view-fields--about.tpl.php
+++ www/sites/all/themes/surreal/template/views/about/views-view-fields--about.tpl.php
@@ -26,7 +26,7 @@
           //print $Title->label_html;
           
           $path = drupal_get_path('theme', 'surreal');
-          $path .= '/images/thinkaps_white.png';
+          $path .= '/images/thinkaps.png';
           
           print theme_image(array('path' => $path, 'width' => 108, 'height' => 49, 'attributes' => array('class' => array('aps'))));
         ?>
