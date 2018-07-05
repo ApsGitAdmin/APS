88371b592064b38d1edd7cd9066647d60efd30e4
Andrew-aps <andrew.morris@thinkaps.com>
2018-07-05 11:34:00 +0100

    Our People Amend V1

--- www/sites/all/themes/surreal/template/views/ourpeople/views-view-fields--our-people.tpl.php
+++ www/sites/all/themes/surreal/template/views/ourpeople/views-view-fields--our-people.tpl.php
@@ -8,11 +8,13 @@ $uri = $fields['uri'];
 $imageinfo = image_get_info($uri->raw);
 $variables = array('style_name' => 'our_people', 'path' => $uri->raw, 'width' => $imageinfo['width'], 'height' => $imageinfo['height'], 'alt' => strip_tags($name));
 $photo = theme_image_style($variables);
+
+$split_name = explode(" ", $name);
 ?>
 <div class="teamBlock column <?php print $zebra; ?>">
       <div class="teamImage" style="background-color:<?php print $colour;?>"><?php print $photo; ?>
         <div class="teamName">
-        	<h2><?php print $name;?></h2>
+        	<h2><div class='minimo-bold'><?php print array_shift($split_name) . "<br/>" . implode(" ", $split_name); ?></div></h2>
 		    <h3><?php print $job;?></h3>
 		    <ul class="socialLinksTeam">
 		    	<li>
